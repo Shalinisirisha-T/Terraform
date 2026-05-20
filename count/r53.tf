dd1e744aa3a60c5a7a0ea741598d3a3408e57ceb
shalini-9393 <shalinithatikonda73@gmail.com>
2026-05-20 11:29:55 +0530

    Update r53.tf

--- count/r53.tf
+++ count/r53.tf
@@ -2,18 +2,18 @@ resource "aws_route53_record" "www" {
   count = 10
   zone_id = var.zone_id
   # interpolation
-  name    = "${var.instances[count.index]}.${var.domain_name}" # mongodb.daws88s.online
+  name    = "${var.instances[count.index]}.${var.domain_name}" # mongodb.shalinidaws88s.online
   type    = "A"
   ttl     = 1
   records = [aws_instance.example[count.index].private_ip]
 }
 
-# roboshop.daws88s.online -> public_ip
+# roboshop.shalinidaws88s.online -> public_ip
 # As part of functions
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
   # interpolation
-  name    = "roboshop.${var.domain_name}" # roboshop.daws88s.online
+  name    = "roboshop.${var.domain_name}" # roboshop.shalinidaws88s.online
   type    = "A"
   ttl     = 1
   records = [aws_instance.example[index(var.instances, "frontend")].public_ip]
